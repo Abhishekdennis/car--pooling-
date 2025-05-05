310c37d1f899e3463add86cc12094dfa0bc348e6
ChiragDawra <chiragdawra46@gmail.com>
2025-05-05 14:26:05 +0530

    more features

--- c.c
+++ c.c
@@ -165,17 +165,17 @@
 //     }
 //     return 0;
 // }
-#include<stdio.h>
-#include<math.h>
-int main (){
-    int rows , columns;
-    printf("Enter the number of rows and columns: ");
-    scanf("%d %d", &rows, &columns);
-    int matrix[rows][columns];
-    printf("Enter the elements of the matrix:\n");
-    for (int i =0; i < rows; i++){
-        for (int j = 0; j < columns; j++){
-            scanf("%d", &matrix[i][j]);
-        }
-    }
-}
+// #include<stdio.h>
+// #include<math.h>
+// int main (){
+//     int rows , columns;
+//     printf("Enter the number of rows and columns: ");
+//     scanf("%d %d", &rows, &columns);
+//     int matrix[rows][columns];
+//     printf("Enter the elements of the matrix:\n");
+//     for (int i =0; i < rows; i++){
+//         for (int j = 0; j < columns; j++){
+//             scanf("%d", &matrix[i][j]);
+//         }
+//     }
+// }
