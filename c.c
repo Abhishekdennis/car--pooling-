d639caf21297ccf2c621ca6a6491ac752ee32d06
Abhishek Dennis <abhishekdennis232006@gmail.com>
2025-05-07 00:05:32 +0530

    Update Code

--- c.c
+++ c.c
@@ -171,6 +171,7 @@ int main() {
 //     int rows , columns;
 //     printf("Enter the number of rows and columns: ");
 //     scanf("%d %d", &rows, &columns);
+//     now we have tAKEN THE OUTPUT FROM THE USER
 //     int matrix[rows][columns];
 //     printf("Enter the elements of the matrix:\n");
 //     for (int i =0; i < rows; i++){
